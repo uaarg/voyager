d47b214ca79e9bd5941bda9166617c969e6ecc1c
hassanfarooq00 <hassanfarooq00@gmail.com>
2024-10-20 02:06:30 -0600

    setup tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Header from './components/Header'
 import Main from './components/Main'
+import './index.css';
 
 function App() {
   return (
--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 const Main: React.FC = () => {
   return (
     <main className="p-6 bg-gray-100">
-      <p className="text-lg">
-        This is the main content area styled with Tailwind CSS.
+      <p className="text-lg font-bold text-xl mb-4 text-red-500">
+        This is the main content area styled with Tailwind .
       </p>
     </main>
   );
